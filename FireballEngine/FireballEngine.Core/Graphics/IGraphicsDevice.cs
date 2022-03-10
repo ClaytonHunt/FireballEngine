9a76c17bb489c86d457eaa2ddb140f386737f1e6
Clayton Hunt <claytonh@itworks.com>
2022-03-09 22:31:32 -0500

    We rendered some color to the canvas Hurray!!

---
+++ FireballEngine/FireballEngine.Core/Graphics/IGraphicsDevice.cs
@@ -0,0 +1,9 @@
+﻿namespace FireballEngine.Core.Graphics
+{
+    public interface IGraphicsDevice
+    {
+        public void Configure(Guid id, int width, int height);
+        public Task Initialize();
+        public Task Clear(float red, float green, float blue, float alpha);
+    }    
+}
---
+++ FireballEngine/FireballEngine.Core/Graphics/PresentationParameters.cs
@@ -0,0 +1,12 @@
+﻿namespace FireballEngine.Core.Graphics
+{
+    public class PresentationParameters
+    {
+        public PresentationParameters()
+        {
+        }
+
+        public int BackbufferWidth { get; set; }
+        public int BackbufferHeight { get; set; }
+    }
+}
